e56c0c55ddfdbf33dfb8359c6a4b2a5bb7b67ace
Srujana <82603720+srujana-16@users.noreply.github.com>
2023-04-20 18:20:24 +0530

    Update TextForm.js

--- frontend/speech-transcription-app/src/components/TextForm.js
+++ frontend/speech-transcription-app/src/components/TextForm.js
@@ -1,72 +1,120 @@
+// The audio chunks and the transcriptions are fetched from the 
+// 'Original data' folder which is present in the public folder
+// Works for any number of transcripts and audio files. 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function TextForm(props) {
     const [text, setText] = useState('Loading...');
+    const [audioSrc, setAudioSrc] = useState(null);
+    const [transcriptNumber, setTranscriptNumber] = useState(1);
+    const [isAudio, setIsAudio] = useState(false);
 
+    // Fetch the transcriptions and audio files 
     useEffect(() => {
         const fetchText = async () => {
             try {
-                const response = await axios.get('blah.txt');
+                const response = await axios.get(`./Original data/transcripts/transcript${transcriptNumber.toString().padStart(4, '0')}.txt`);
                 setText(response.data);
+                setIsAudio(false);
             } catch (error) {
                 console.error(error);
             }
         };
 
-        fetchText();
-    }, []);
+        const fetchAudio = async () => {
+            try {
+                const response = await axios.get(`${process.env.PUBLIC_URL}/Original data/audio_chunks/chunk${transcriptNumber.toString().padStart(4, '0')}.wav`, {
+                    responseType: 'blob'
+                });
+                const blob = new Blob([response.data]);
+                const url = URL.createObjectURL(blob);
+                setAudioSrc(url);
+                setIsAudio(true);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-    console.log(text);
+        if (props.isAudio) {
+            fetchAudio();
+        } else {
+            fetchText();
+        }
+    }, [transcriptNumber, props.isAudio]);
 
-    const handleMarkForReview = () => {
-        let newText = text.toUpperCase();
-        setText(newText)
-        props.showAlert("Converted to uppercase!", "success");
-    }
-
+    // Save button
     const handleSave = () => {
         let newText = '';
         setText(newText);
-        props.showAlert("Text Cleared!", "success");
-    }
+        props.showAlert('Text Cleared!', 'success');
+    };
 
     const handleOnChange = (event) => {
-        setText(event.target.value)
-    }
+        setText(event.target.value);
+    };
 
+    // Discard button
     const handleDiscard = () => {
-        navigator.clipboard.writeText(text);
-        props.showAlert("Copied to Clipboard!", "success");
-    }
+        if (isAudio) {
+            navigator.clipboard.writeText('');
+        } else {
+            navigator.clipboard.writeText(text);
+        }
+        props.showAlert('Copied to Clipboard!', 'success');
+    };
 
+    // Next button
     const handleNext = () => {
-        let newText = text.split(/[ ]+/);
-        setText(newText.join(" "));
-        props.showAlert("Extra spaces removed!", "success");
-    }
+        props.handleNext();
+        setTranscriptNumber(transcriptNumber + 1);
+        setText('Loading...');
+        setAudioSrc(null);
+    };
+
+    // Previous button
+    const handlePrevious = () => {
+        props.handlePrevious();
+        setTranscriptNumber((prevNumber) => prevNumber - 1);
+        setText('Loading...');
+        setAudioSrc(null);
+    };
+
 
     return (
         <>
-            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+            <div
+                className='container'
+                style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}
+            >
                 <h3 className='mb-4'>{props.heading}</h3>
-                <div className="mb-3">
+                <div className='mb-3' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                     <textarea
-                        className="form-control"
-                        style={{ backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }}
-                        id="myBox"
-                        rows="8"
+                        className='form-control'
+                        style={{
+                            backgroundColor: props.mode === 'dark' ? '#13466e' : 'white',
+                            color: props.mode === 'dark' ? 'white' : '#042743',
+                        }}
+                        id='myBox'
+                        rows='8'
                         onChange={handleOnChange}
                         value={text}
-                    ></textarea>
+                    />
+                    {isAudio && (
+                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem', width: '100%', height: '100%' }}>
+                            <audio controls src={audioSrc} />
+                        </div>
+                    )}
                 </div>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleMarkForReview}>Mark for Review</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleSave}>Save</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleDiscard}>Discard</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleNext}>Next</button>
+                <div className="d-flex justify-content-center">
+                    <button disabled={text === 'Loading...' || text.length === 0} className='btn btn-primary mx-1 my-1' onClick={handleSave}style={{ backgroundColor:"limegreen" }}>Save</button>
+                    <button disabled={text === 'Loading...' || text.length === 0} className='btn btn-primary mx-1 my-1' onClick={handleDiscard}style={{ backgroundColor:"Red" }}>Discard</button>
+                    <button className='btn btn-primary mx-1 my-1' onClick={handlePrevious} disabled={text === 'Loading...' || transcriptNumber <= 1}>Previous</button>
+                    <button className='btn btn-primary mx-1 my-1' onClick={handleNext} disabled={text === 'Loading...'}>Next</button>
+
+                </div>
             </div>
         </>
     );
+
 }
-
-
