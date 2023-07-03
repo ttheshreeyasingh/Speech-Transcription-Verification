cb5c06dde0bf9e6bbdc41c882af8fcc686cb237b
Srujana <82603720+srujana-16@users.noreply.github.com>
2023-07-03 15:47:41 +0530

    Update App.js

--- frontend/speech-transcription-app/src/App.js
+++ frontend/speech-transcription-app/src/App.js
@@ -1,49 +1,58 @@
+import React, { useState } from 'react';
 import "./App.css";
 import Navbar from './components/Navbar';
 import AudioPlayerWithTextForm from './components/AudioPlayerWithTextForm';
-import React, { useState } from 'react';
 import Alert from './components/Alert';
+import Login from './components/Login';
 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-    }
-    else{
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
   return (
     <>
       <Navbar title="Speech Transcription Verification App" mode={mode} toggleMode={toggleMode} key={new Date()} />
       <Alert alert={alert} />
-  
+
       <div className="App container py-3">
-        <h3>Verify the transcription of the below audio chunk</h3>
+        {isLoggedIn ? (
+          <>
+            <h3>Verify the transcription of the below audio chunk</h3>
+            <AudioPlayerWithTextForm showAlert={showAlert} mode={mode} />
+          </>
+        ) : (
+          <Login onLogin={handleLogin} />
+        )}
       </div>
-
-      <AudioPlayerWithTextForm showAlert={showAlert} mode={mode} />
-
     </>
   );
-}  
+}
 
 export default App;
