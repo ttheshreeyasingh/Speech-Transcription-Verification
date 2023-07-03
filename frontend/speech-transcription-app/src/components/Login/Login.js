3d988b42a03e4cc2a1bd33cd8ba897d9a22a2700
Srujana <82603720+srujana-16@users.noreply.github.com>
2023-07-03 15:53:55 +0530

    Upload login.js and login.css

---
+++ frontend/speech-transcription-app/src/components/Login/Login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBInput} from 'mdb-react-ui-kit';
+import './Login.css';
+
+function Login({ onLogin }) {
+
+  return (
+    <MDBContainer fluid className="p-3 my-5 h-custom">
+
+      <MDBRow>
+
+        <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image" />
+        </MDBCol>
+
+        <MDBCol col='4' md='6'>
+
+          <div className="divider d-flex align-items-center my-4">
+            <p className="text-center fw-bold mx-3 mb-0">Or</p>
+          </div>
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+          <div className='text-center text-md-start mt-4 pt-2'>
+            <MDBBtn className="mb-0 px-5" size='lg'>Login</MDBBtn>
+            <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href="#!" className="link-danger">Sign up</a></p>
+          </div>
+
+        </MDBCol>
+
+      </MDBRow>
+
+    </MDBContainer>
+  );
+}
+
+export default Login;
