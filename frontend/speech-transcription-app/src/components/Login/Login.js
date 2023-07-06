f97287dac125744195036106278f329ecf4873ea
Srujana <82603720+srujana-16@users.noreply.github.com>
2023-07-06 23:46:38 +0530

    Add selecting audio option

--- frontend/speech-transcription-app/src/components/Login/Login.js
+++ frontend/speech-transcription-app/src/components/Login/Login.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 function Login({ onLogin }) {
+  const [audioFiles, setAudioFiles] = useState(['audio1.mp3', 'audio2.mp3', 'audio3.mp3']); // Initial audio file names
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      // Add the uploaded file to the dropdown options
+      const fileName = selectedFile.name;
+      setAudioFiles((prevFiles) => [...prevFiles, fileName]);
+    }
+  };
+
   return (
     <div className="login-container">
-      <div className="login-image">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample" />
-      </div>
+      <div className="login-image-bg"></div>
 
       <div className="login-form">
+        <h2 className="login-heading">Login</h2>
+
         <div className="divider d-flex align-items-center my-4">
           {/* <p className="text-center fw-bold mx-3 mb-0">Or</p> */}
         </div>
 
         <div className="mb-4">
-          <input className="form-control" type="email" placeholder="Email address" />
+          <input className="form-control" type="name" placeholder="Name" />
         </div>
         <div className="mb-4">
-          <input className="form-control" type="password" placeholder="Password" />
+          <input className="form-control" type="email" placeholder="Email Address" />
+        </div>
+        <div className="mb-4">
+          <select className="form-control">
+            <option value="">Select Audio File</option>
+            {audioFiles.map((file, index) => (
+              <option key={index} value={file}>{file}</option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <input type="file" onChange={handleFileChange} />
+          <button className="btn btn-primary" onClick={handleUpload}>Upload</button>
         </div>
 
         <div className="text-center text-md-start mt-3">
           <button className="btn btn-primary" onClick={onLogin}>Login</button>
-          <p className="mt-2">
-            <a href="#" className="link-danger">Forgot Password?</a>
-          </p>
         </div>
       </div>
     </div>
