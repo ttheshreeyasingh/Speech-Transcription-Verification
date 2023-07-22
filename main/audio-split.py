d529794e1b903f0578d37173f91f4ec0522a0253
Srujana <82603720+srujana-16@users.noreply.github.com>
2023-07-23 00:32:33 +0530

    Implement audio file selection from multiple audios
    > on selecting audio file from login page, server uses this audio file to run audio split and transcription. 
    >save and discard needs to be implemented

--- main/audio-split.py
+++ main/audio-split.py
@@ -1,28 +1,22 @@
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
 import os
 import collections
 import contextlib
-import sys
 import wave
-import os
 import webrtcvad
+import sys
+
 
 def read_wave(path):
-
     with contextlib.closing(wave.open(path, 'rb')) as wf:
         num_channels = wf.getnchannels()
-        assert num_channels == 1
+        assert num_channels in (1, 2)  # Allowing 1 or 2 channels
         sample_width = wf.getsampwidth()
         assert sample_width == 2
         sample_rate = wf.getframerate()
-        assert sample_rate in (8000, 16000, 32000, 48000)
         pcm_data = wf.readframes(wf.getnframes())
-        return pcm_data, sample_rate
-
+        return pcm_data, sample_rate, num_channels
 
 def write_wave(path, audio, sample_rate):
-
     with contextlib.closing(wave.open(path, 'wb')) as wf:
         wf.setnchannels(1)
         wf.setsampwidth(2)
@@ -31,17 +25,13 @@ def write_wave(path, audio, sample_rate):
         frames = wf.getnframes()
         return frames / float(sample_rate)
 
-
 class Frame(object):
-
     def __init__(self, bytes, timestamp, duration):
         self.bytes = bytes
         self.timestamp = timestamp
         self.duration = duration
 
-
 def frame_generator(frame_duration_ms, audio, sample_rate):
-
     n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
     offset = 0
     timestamp = 0.0
@@ -51,55 +41,34 @@ def frame_generator(frame_duration_ms, audio, sample_rate):
         timestamp += duration
         offset += n
 
-
-def vad_collector(sample_rate, frame_duration_ms,
-                  padding_duration_ms, vad, frames):
-
+def vad_collector(sample_rate, frame_duration_ms, padding_duration_ms, vad, frames):
     num_padding_frames = int(padding_duration_ms / frame_duration_ms)
     ring_buffer = collections.deque(maxlen=num_padding_frames)
     triggered = False
-
     voiced_frames = []
     for frame in frames:
         is_speech = vad.is_speech(frame.bytes, sample_rate)
-
-
         if not triggered:
             ring_buffer.append((frame, is_speech))
             num_voiced = len([f for f, speech in ring_buffer if speech])
-
             if num_voiced > 0.9 * ring_buffer.maxlen:
                 triggered = True
-
-
                 for f, s in ring_buffer:
                     voiced_frames.append(f)
                 ring_buffer.clear()
         else:
-
             voiced_frames.append(frame)
             ring_buffer.append((frame, is_speech))
             num_unvoiced = len([f for f, speech in ring_buffer if not speech])
-
             if num_unvoiced > 0.9 * ring_buffer.maxlen:
-
                 triggered = False
                 yield b''.join([f.bytes for f in voiced_frames])
                 ring_buffer.clear()
                 voiced_frames = []
     if triggered:
         pass
-
     if voiced_frames:
         yield b''.join([f.bytes for f in voiced_frames])
-
-path = "./frontend/speech-transcription-app/public/Original data"
-if not os.path.exists(path):
-        os.makedirs(path)
-        print("Output folder created")
-else:
-        print("Output folder already present")
-        sys.exit()
 
 def folder(path):
     if not os.path.exists(path):
@@ -108,52 +77,34 @@ def folder(path):
     else:
         print("Output folder already present")
 
-path = "./frontend/speech-transcription-app/public/Original data"
-folder(path)
-path = "./main/save"
-folder(path)
-path = "./main/discard"
-folder(path)
+def main():
+    # Check if the correct number of command-line arguments is passed
+    if len(sys.argv) != 3:
+        print("Usage: python audio_split.py <input_file_name> <output_path>")
+        sys.exit(1)
 
-file_name= "./main/mod_1.wav"
-op_path= "./frontend/speech-transcription-app/public/Original data/audio_chunks"
+    file_name = sys.argv[1]
+    op_path = sys.argv[2]
 
-
-
-def main(file_name,op_path):
-
-    if os.path.isdir(op_path):
-        print("Output folder already present")
+    if not os.path.exists(op_path):
+        os.makedirs(op_path)
+        print("Output folder created")
     else:
-        os.mkdir(op_path)
-        print("Output folder created")  
-    
-    audio, sample_rate = read_wave(file_name)
+        print("Output folder already present")
+
+    audio, sample_rate, _ = read_wave(file_name)
     vad = webrtcvad.Vad(2)
     frames = frame_generator(30, audio, sample_rate)
     segments = vad_collector(sample_rate, 30, 300, vad, frames)
 
+    file_name_without_extension = os.path.splitext(os.path.basename(file_name))[0]
+    audio_folder_path = os.path.join(op_path, file_name_without_extension, "audio_chunks")
+    folder(audio_folder_path)
+
     for i, segment in enumerate(segments):
-        path = op_path+'/'+'chunk%004d.wav' % (i+1,)
-        print(' Writing %s' %(path,))
-        write_wave(path, segment, sample_rate )
+        path = os.path.join(audio_folder_path, f'chunk{str(i+1).zfill(4)}.wav')
+        print(f'Writing {path}')
+        write_wave(path, segment, sample_rate)
 
-
-
-# sys.argv[1]
-
-# sys.argv[2]
-file_name= "./main/mod_1.wav"
-op_path= "./frontend/speech-transcription-app/public/Original data/audio_chunks"
-main(file_name,op_path)
-
-print("Audio Splitting Done")
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
