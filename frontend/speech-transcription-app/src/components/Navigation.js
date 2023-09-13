00956d2d5d32a07ab9576171305149e51115ef34
sruj <srujana.vanka@students.iiit.ac.in>
2023-09-13 13:05:06 +0530

    Add navigation search and improve UI

--- frontend/speech-transcription-app/src/components/Navigation.js
+++ frontend/speech-transcription-app/src/components/Navigation.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css'; // Import your own CSS for custom styling
+import '../App.css';
 
 const Navigation = ({ numberOfChunks, handleChunkSelect }) => {
+  const [searchQuery, setSearchQuery] = useState(''); 
+
   const chunkLinks = Array.from({ length: numberOfChunks }, (_, index) => (
     <li key={index}>
       <button className="chunk-button" onClick={() => handleChunkSelect(index + 1)}>
-
         Chunk {index + 1}
       </button>
     </li>
@@ -14,11 +15,30 @@ const Navigation = ({ numberOfChunks, handleChunkSelect }) => {
 
   return (
     <div className="navigation">
-      <h2 className="navigation-heading">Audio Chunks</h2>
-      <ul className="chunk-list">{chunkLinks}</ul>
+      <h2 className="navigation-heading">Transcript Navigation</h2>
+
+      {/* search bar */}
+      <input
+        type="text"
+        className="search-bar" 
+        placeholder="Search for a chunk"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
+      <ul className="chunk-list">
+        {chunkLinks
+          .filter((_, index) =>
+            searchQuery
+              ? `Chunk ${index + 1}`.toLowerCase().includes(searchQuery.toLowerCase())
+              : true
+          )
+          .map((chunkLink, index) => (
+            <React.Fragment key={index}>{chunkLink}</React.Fragment>
+          ))}
+      </ul>
     </div>
   );
 };
-
 
 export default Navigation;
